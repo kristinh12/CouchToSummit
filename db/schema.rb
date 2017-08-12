3e76ea09aeb76b2231237c62774916cad2cdbf84
Kristin Heininger <dauphindouze@comcast.net>
2017-08-12 02:16:34 +0000

    Added length to trails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170811201041) do
+ActiveRecord::Schema.define(version: 20170812014556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170811201041) do
     t.integer  "elevation_gain"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.float    "length"
   end
 
   create_table "users", force: :cascade do |t|
