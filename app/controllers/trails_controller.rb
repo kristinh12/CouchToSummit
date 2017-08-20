4514e25234cfbf1368a36f983cd5e9eb3b740804
Kristin Heininger <dauphindouze@comcast.net>
2017-08-20 02:21:15 +0000

    Added trail show page

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,11 +1,14 @@
 class TrailsController < ApplicationController
 
-def index
-  @trails = Trail.all
-end
+  def index
+    @trails = Trail.all
+  end
 
-def show
-  @trail = Trail.find(params:[:id])
-end
+  def show
+    @trail = Trail.find_by_id(params[:id])
+    if @trail.nil?
+      render plain: 'Not Found', status: :not_found
+    end
+  end
 
 end
--- spec/controllers/trails_controller_spec.rb
+++ spec/controllers/trails_controller_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe TrailsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'trails#show' do
+    it 'should show the trail page' do
+      trail = Trail.create(
+        name: 'fake trail',
+        length: 2.3,
+        elevation_gain: 1000,
+        highest_elevation: 1200
+        )
+      get :show, params: {id: trail.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'should return a 404 error if the trail is not found' do
+      get :show, params: {id: 'tacocat' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
 end
